d302e49b9f42f123b9e899677f949aab7f81a3f0
rishi <104455207+rr1028@users.noreply.github.com>
2024-06-30 21:10:46 +0530

    c4

---
+++ websgovscrapper.R
@@ -0,0 +1,42 @@
+library(rvest)
+library(httr)
+library(tidyverse)
+
+# Define the target URL
+url <- "https://results.eci.gov.in/PcResultGenJune2024/index.htm"
+
+# Set the user agent to mimic a browser
+response <- GET(url, add_headers('User-Agent' = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'))
+
+# Check the status of the response
+status_code(response)
+
+# If the status is 200, proceed with scraping
+if (status_code(response) == 200) {
+  # Read the HTML content of the webpage
+  webpage <- content(response, as = "text")
+  webpage <- read_html(webpage)
+  
+  # Use CSS selectors to extract the data you want
+  party <- html_nodes(webpage, "tr > :nth-child(1)") %>% html_text()
+  
+  
+  
+  
+  
+  # Check the number of columns of data per scrape match
+  length(titles)
+  
+  # Combine the extracted data into a data frame
+  data <- data.frame(
+    party = party
+    
+    )
+  
+  # View the scraped data
+  view(data)
+  write.table(data, file = "scraped_data.txt", sep = "\t", row.names = FALSE, quote = FALSE)
+} else {
+  print("Failed to retrieve the webpage.")
+}
+
